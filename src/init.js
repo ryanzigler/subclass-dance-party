c990e8592a4874e3661a417ffba782ce17413170
Ryan Zigler <rpzigler@gmail.com>
2020-07-09 14:31:01 -0500

    Working on implementing lineup

--- src/init.js
+++ src/init.js
@@ -2,91 +2,58 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addBlinkyDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    // make a dancer with a random position
-
     var newBlinkyDancer = new MakeBlinkyDancer(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(newBlinkyDancer);
     $('body').append(newBlinkyDancer.$node);
   });
 
   $('.addDancingBabyButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    // make a dancer with a random position
-
     var newDancingBaby = new MakeDancingBaby(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(newDancingBaby);
     $('body').append(newDancingBaby.$node);
   });
-});
 
+  $('.addElaineDanceButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
+    var dancerMakerFunction = window[dancerMakerFunctionName];
 
-$('.addElaineDanceButton').on('click', function(event) {
-  /* This function sets up the click handlers for the create-dancer
-   * buttons on dancefloor.html. You should only need to make one small change to it.
-   * As long as the "data-dancer-maker-function-name" attribute of a
-   * class="addDancerButton" DOM node matches one of the names of the
-   * maker functions available in the global scope, clicking that node
-   * will call the function to make the dancer.
-   */
+    var newDancingElaine = new MakeElaineDance(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
+      Math.random() * 1000
+    );
+    window.dancers.push(newDancingElaine);
+    $('body').append(newDancingElaine.$node);
+  });
 
-  /* dancerMakerFunctionName is a string which must match
-   * one of the dancer maker functions available in global scope.
-   * A new object of the given type will be created and added
-   * to the stage.
-   */
-  var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
-  // get the maker function for the kind of dancer we're supposed to make
-  var dancerMakerFunction = window[dancerMakerFunctionName];
-
-  // make a dancer with a random position
-
-  var newDancingElaine = new MakeElaineDance(
-    $("body").height() * Math.random(),
-    $("body").width() * Math.random(),
-    Math.random() * 1000
-  );
-  $('body').append(newDancingElaine.$node);
+  $('.lineUpDancers').on('click', function(event) {
+    // var index = 0;
+    // for (var i = 0; i < window.dancers.length; i++) {
+    //   $(window.dancers[i]).animate({
+    //     top: 100,
+    //     left: 100 + index
+    //   });
+    //   index += 50;
+    // }
+    for (var i = 0; i < window.dancers.length; i++) {
+      window.dancers[i].lineUp();
+    }
+  });
 });
